b62a9fe88ca3dfa393f7d6b5a436772e248bf72f
comus3 <come.plantin.carrenard@gmail.com>
2023-12-12 10:19:15 +0100

    obselete message

--- schoolManager/DAL/Repositories/Table1.cs
+++ schoolManager/DAL/Repositories/Table1.cs
@@ -1,6 +1,15 @@
 //!!!EXEMPLE!!!
 // NE PAS TOuCHER
 // Table1Repository.cs
+
+
+// OBSELETE
+//
+// This is the old version (at least what we started)
+// the app now is implemented using json instead of 
+// sql for facility but the code is here for potential
+// future implementation
+
 using schoolManager.Models; // Import your entity model
 
 namespace schoolManager.DAL.Repositories
