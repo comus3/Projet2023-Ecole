4c01cd92eb8fbabdf0aa658305ba2600c593e7d6
Soraya-j <21255@ecam.be>
2023-12-13 11:58:37 +0100

    About Page

---
+++ schoolManager/ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace schoolManager.ViewModels;
+
+internal class AboutViewModel
+{
+    public string Title => AppInfo.Name; 
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
+}
