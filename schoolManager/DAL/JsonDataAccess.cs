ce3846e5d72692cd77917b1a53d70ab1ae437d4d
comus3 <come.plantin.carrenard@gmail.com>
2023-12-13 10:33:49 +0100

    load data method

--- schoolManager/DAL/JsonDataAccess.cs
+++ schoolManager/DAL/JsonDataAccess.cs
@@ -5,12 +5,10 @@
 //chaque key du dico ext est le type d'objet,
 //chaque chaque value du dico ext est une instance de l'objet(rep par un dico)
 //chaque value-key int du dico rep ses attributs
-
-
-
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace schoolManager.DAL
 {
@@ -25,21 +23,32 @@ namespace schoolManager.DAL
 
         public Dictionary<string, Dictionary<string, object>> LoadData()
         {
-            if (File.Exists(filePath))
+            try
             {
-                var json = File.ReadAllText(filePath);
-                return JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, object>>>(json);
+                if (File.Exists(filePath))
+                {
+                    var json = File.ReadAllText(filePath);
+                    return JsonConvert.DeserializeObject<Dictionary<string, List<Dictionary<string,string>>>>(json);
+                }
+                else
+                {
+                    throw new Exception($"File not found at path: {filePath}");
+                }
             }
-            else
+            catch (JsonException ex)
             {
-                return new Dictionary<string, Dictionary<string, object>>();
+                Console.WriteLine($"Error deserializing JSON from file {filePath}: {ex.Message}");
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                return -1;
             }
         }
 
-        public void SaveData(Dictionary<string, Dictionary<string, object>> data)
-        {
-            var json = JsonSerializer.Serialize(data);
-            File.WriteAllText(filePath, json);
-        }
+
+
+
     }
 }
