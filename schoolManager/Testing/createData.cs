5fca3413d71fd593b70b367370057606c856040c
Soraya-j <21255@ecam.be>
2023-12-13 10:12:54 +0100

    update folder testing

--- schoolManager/Testing/createData.cs
+++ schoolManager/Testing/createData.cs
@@ -1,22 +1,22 @@
+using schoolManager.Models;
+
 namespace schoolManager.Testing{
     class Testing{
         public void init(){
-            Models.Enseignant Lurking = new Models.Enseignant(10000,"Quentin","Lurk1");
-            Models.Enseignant DLH = new Models.Enseignant(15000,"Quentin","Delhaye");
+            Enseignant Lurking = new Enseignant(10000,"Quentin","Lurk1");
+            Enseignant DLH = new Enseignant(15000,"Quentin","Delhaye");
 
-            Models.Activite Info = new Models.Activite("INFO","INFO3000",Lurking,5);
-            Models.Activite Elec = new Models.Activite("ELEC ANAL","ANAL3000",DLH,5);
-            Models.Activite ElecNum = new Models.Activite("ELEC NUM","NUM3000",DLH,3);
+            Activite Info = new Activite("INFO","INFO3000",Lurking,5);
+            Activite Elec = new Activite("ELEC ANAL","ANAL3000",DLH,5);
+            Activite ElecNum = new Activite("ELEC NUM","NUM3000",DLH,3);
 
-            List<Models.Eval> evaluations = new List<Models.Eval>{
-                new Models.Appreciation("TB",Info),
-                new Models.Appreciation("B",Elec),
-                new Models.Cote(14,ElecNum)
+            List<Eval> evaluations = new List<Eval>{
+                new Appreciation("TB",Info),
+                new Appreciation("B",Elec),
+                new Cote(14,ElecNum)
             };
  
-            Models.Etudiant Baptiste = new Models.Etudiant("Batou","Chouchou",evaluations);
-
-            Console.WriteLine(Baptiste.Bulletin());
+            Etudiant Baptiste = new Etudiant("Batou","Chouchou",evaluations);
         }
     }
 }
