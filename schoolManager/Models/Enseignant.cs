20533282d653bcbf643c480be608f4c65572d312
Côme Plantin-Carrenard <120224883+comus3@users.noreply.github.com>
2023-12-15 10:03:55 +0100

    Upnew list init

--- schoolManager/Models/Enseignant.cs
+++ schoolManager/Models/Enseignant.cs
@@ -3,7 +3,7 @@
 public class Enseignant : Person
 {
     private int salaire;
-    private static List<Enseignant> listEnseignant;
+    private static List<Enseignant> listEnseignant = new List<Enseignant>{};
 
     public Enseignant(int salaire,string firstName,string lastName) :
     base(firstName,lastName)
