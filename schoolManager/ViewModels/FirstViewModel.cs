00b5652861caab867296c3fd96205079660c2221
Soraya-j <21255@ecam.be>
2023-12-15 10:29:46 +0100

    fixing struct Fpage

--- schoolManager/ViewModels/FirstViewModel.cs
+++ schoolManager/ViewModels/FirstViewModel.cs
@@ -4,22 +4,16 @@ using System.Windows.Input;
 using schoolManager.Models;
 namespace schoolManager.ViewModels;
 
-public class FirstViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public Etudiant Baptiste { get; set; }
-
-        public FirstViewModel()
-        {
-            Baptiste = new Etudiant("Batou", "Chouchou", new List<Evaluation>());
-        }
-    // public string Title1 => "Affichage";
-    // //    Enseignant.getListEnseignant();
-    // // Enseignant.getListEnseignant(){0}.attrigbut1
+internal class FirstViewModel
+{
+    public string Title => "Affichage";
+    //Enseignant.getListEnseignant();
+    // Enseignant.getListEnseignant(){0}.attrigbut1
 
     
-    // public string Message => "Liste des Enseignants";
-    // public ICommand ShowMoreInfoCommand { get; }
+    public string Message => "Liste des Enseignants";
+
+
     
 }
+
--- schoolManager/Views/FirstPage.xaml.cs
+++ schoolManager/Views/FirstPage.xaml.cs
@@ -1,14 +1,66 @@
-using Xamarin.Forms.Xaml;
+namespace schoolManager.Views;
 
-namespace schoolManager.Views
+public partial class FirstPage : ContentPage
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FirstPage : ContentPage
+    public FirstPage()
     {
-        public FirstPage()
-        {
-            InitializeComponent();
-            BindingContext = new FirstViewModel();
-        }
+        InitializeComponent();
     }
 }
+// using Microsoft.Maui.Controls;
+
+// namespace schoolManager.Views;
+
+//     public partial class FirstPage : ContentPage
+//     {
+//         TableView enseignantsTable, activitesTable, etudiantsTable;
+
+//         public FirstPage()
+//         {
+//             InitializeUI();
+//         }
+
+//         void InitializeUI()
+//         {
+//             enseignantsTable = new TableView
+//             {
+//                 HasUnevenRows = true,
+//                 Root = new TableRoot
+//                 {
+//                     new TableSection("Enseignants")
+//                     {
+//                         // Ajoutez les Enseignants ici
+//                     }
+//                 }
+//             };
+
+//             activitesTable = new TableView
+//             {
+//                 HasUnevenRows = true,
+//                 Root = new TableRoot
+//                 {
+//                     new TableSection("Activités")
+//                     {
+//                         // Ajoutez les Activités ici
+//                     }
+//                 }
+//             };
+
+//             etudiantsTable = new TableView
+//             {
+//                 HasUnevenRows = true,
+//                 Root = new TableRoot
+//                 {
+//                     new TableSection("Étudiants")
+//                     {
+//                         // Ajoutez les Étudiants ici
+//                     }
+//                 }
+//             };
+
+//             Content = new StackLayout
+//             {
+//                 Children = { enseignantsTable, activitesTable, etudiantsTable }
+//             };
+//         }
+//     }
