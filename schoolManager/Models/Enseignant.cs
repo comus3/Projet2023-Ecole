89ca3640639584666a988b160e0d23932832dacc
comus3 <come.plantin.carrenard@gmail.com>
2023-12-18 01:56:56 +0100

    enseignant uid, findenseignant,get uid

--- schoolManager/Models/Enseignant.cs
+++ schoolManager/Models/Enseignant.cs
@@ -1,14 +1,29 @@
-﻿namespace schoolManager.Models
+﻿using schoolManager.Services;
+
+namespace schoolManager.Models
 {
     public class Enseignant : Person
     {
+        private string Uid { get; set;}
         private int salaire;
         private static List<Enseignant> listEnseignant = new List<Enseignant>();
+
+        public static Enseignant findEnseignant(int UID)
+        {
+            for (Enseignant enseignant in listEnseignant)
+            {
+                if (enseignant.Uid == UID)
+                {
+                    return enseignant;
+                }
+            }
+        }
 
         public Enseignant(int salaire, string firstName, string lastName) :
             base(firstName, lastName)
         {
             this.salaire = salaire;
+            Uid = Guid.GenerateNewUid(listEnseignant);
             listEnseignant.Add(this);
         }
 
