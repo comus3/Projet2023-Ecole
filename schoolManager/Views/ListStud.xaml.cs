487d60011f7760f0eb3f49be2cebb4c53365258f
Soraya-j <21255@ecam.be>
2023-12-21 21:06:07 +0100

    affichage etudiant

--- schoolManager/Views/ListStud.xaml.cs
+++ schoolManager/Views/ListStud.xaml.cs
@@ -1,3 +1,5 @@
+using schoolManager.Models;
+
 namespace schoolManager.Views;
 
 public partial class ListStud : ContentPage
@@ -5,7 +7,11 @@ public partial class ListStud : ContentPage
     public ListStud()
     {
         InitializeComponent();
+        BindingContext = this;
     }
+
+    public List<Etudiant> studList = Etudiant.ListEtudiant;
+    public List<Etudiant> StudList {get {return studList;}}
     private void Button_Home(object sender, EventArgs e)
 	{
         Navigation.PushAsync(new MainPage());   
